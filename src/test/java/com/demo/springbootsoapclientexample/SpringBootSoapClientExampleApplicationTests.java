d31d38b54e9f73cc01a3b1d60015a7bd402a04aa
Amol.Sarwade <amol.sarwade@digitaleconomysolutions>
2020-06-23 17:29:02 +0400

    initial commit

---
+++ src/test/java/com/demo/springbootsoapclientexample/SpringBootSoapClientExampleApplicationTests.java
@@ -0,0 +1,13 @@
+package com.demo.springbootsoapclientexample;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringBootSoapClientExampleApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
